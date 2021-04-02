3a536c4d54d98248b94cf8221cb1223df9b8d0f2
kamome-829 <tokunaga8695@gmail.com>
2021-04-02 21:41:53 +0900

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD5Rv0MeCQV2d2oCXODs6teTl5NJl99npk",
+    authDomain: "eat-a7ef3.firebaseapp.com",
+    projectId: "eat-a7ef3",
+    storageBucket: "eat-a7ef3.appspot.com",
+    messagingSenderId: "779481246498",
+    appId: "1:779481246498:web:5a78c6cf92a6a7a7b8537b",
+    measurementId: "G-KDMFE2M0CL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
